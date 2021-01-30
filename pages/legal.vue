33ca2a56becd245bfb4dabdd062134155d32fa4e
Derek Kaplan <thederekkaplan@protonmail.com>
2021-01-30 12:52:55 -0500

    Legal page

---
+++ pages/legal.vue
@@ -0,0 +1,10 @@
+<template>
+	<a-layout-content>
+		<h1>Legal Information</h1>
+		<p>To learn more about the YACU and its structure, please view our <a href="https://bylaws.yacu.org">bylaws</a> and our internal <a href="https://docs.google.com/document/d/e/2PACX-1vQHKoxfKOBv5nYuJWQGkH5y0nMWMdL-_1gN9QOOfTwS3yd3BWi4ZoZwJ5It3ZQqjR8U2SxfWXU8yKGs/pub">Code of Conduct</a>. For further questions regarding the content on this page, please email <a href="mailto:legal@yacu.org">legal@yacu.org</a>.</p>
+		<h3><a href="/Terms%20Of%20Use.pdf"><a-icon type="file-pdf"/> Terms of Use</a></h3>
+		<h3><a href="/Privacy%20Policy.pdf"><a-icon type="file-pdf"/> Privacy Policy</a></h3>
+		<h2>Conflict of Interest Disclosures</h2>
+		<iframe src="https://airtable.com/embed/shrfVdpsGYYwYe94N?backgroundColor=purple&amp;viewControls=on" width="100%" height="1000px" frameborder="0"/>
+	</a-layout-content>
+</template>
