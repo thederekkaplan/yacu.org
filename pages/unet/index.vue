2d5c136c72e09a5d1162ff5019f0740913fb3be3
Derek Kaplan <thederekkaplan@protonmail.com>
2021-02-02 09:06:04 -0500

    Added cover photos for unet posts

--- pages/unet/index.vue
+++ pages/unet/index.vue
@@ -8,7 +8,7 @@
 				<a-col v-for="item in articles" :key="item.href" span="8">
 					<nuxt-link :to="'/unet/' + item.href">
 		    			<a-card hoverable :bordered="false" style="height: 100%">
-		    				<!--<img slot="cover" :src="item.src"/>-->
+		    				<img slot="cover" :src="item.src"/>
 		    				<h3>{{ item.title }}</h3>
 							<a-card-meta :description="'By ' + item.author" />
 						</a-card>
@@ -35,8 +35,31 @@ export default {
   			title: data.title,
   			author: data.author,
   			href: data.url,
+        srcName: data.image,
   		});
-  	})
-  }
+  	});
+    if (process.browser) {
+      this.loadPictures();
+    }
+  },
+  methods: {
+    async loadPictures () {
+      const addSrc = async item => {
+        if(item.srcName) {
+          const url = await this.$fire.storage.ref('unet').child(item.srcName).getDownloadURL();
+          return { ...item, src: url };
+        } else {
+          return { ...item, src: '' };
+        }
+      }
+
+      this.articles = await Promise.all(this.articles.map(addSrc));
+    },
+  },
+  mounted () {
+    if (!this.$fetchState.pending) {
+      this.loadPictures();
+    }
+  },
 }
 </script>
