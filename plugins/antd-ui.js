8b6843401263d7730498373aea8f2540234fb8e7
Derek Kaplan <thederekkaplan@protonmail.com>
2021-02-02 11:46:39 -0500

    Importing ant design components individually

--- plugins/antd-ui.js
+++ plugins/antd-ui.js
@@ -1,4 +1,18 @@
 import Vue from 'vue'
-import Antd from 'ant-design-vue/lib'
+import { Layout, Row, Col, Menu, Divider, Spin, List, Carousel, Statistic, Button, Card, Collapse, Affix, Icon, Avatar } from 'ant-design-vue/lib'
 
-Vue.use(Antd)
+Vue.use(Layout);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Menu);
+Vue.use(Divider);
+Vue.use(Spin);
+Vue.use(List);
+Vue.use(Carousel);
+Vue.use(Statistic);
+Vue.use(Button);
+Vue.use(Card);
+Vue.use(Collapse);
+Vue.use(Affix);
+Vue.use(Icon);
+Vue.use(Avatar);
