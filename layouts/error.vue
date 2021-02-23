9698cb8047474dc1564f87eb5272039d39e71c0f
Derek Kaplan <thederekkaplan@protonmail.com>
2021-02-23 18:32:18 -0500

    Better 404 page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,10 @@
 <template>
 	<a-layout-content>
-		404
+		<br>
+		<h3>Sorry, we couldn't find that page.</h3>
+		<p>Error 404</p>
+		<br>
+		<nuxt-link to="/"><a-button type="primary" size="large">Home</a-button></nuxt-link>
 	</a-layout-content>
 </template>
 
