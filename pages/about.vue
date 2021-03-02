c26e98a428f3af506fbb5f1950ecf88c77ac9efe
Derek Kaplan <thederekkaplan@protonmail.com>
2021-03-01 21:21:16 -0500

    Reworded about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,7 @@
 <template>
 	<a-layout-content>
 		<h1>About the YACU</h1>
-		<p>The Young Americans Coalition for Unity (YACU) is empowering young people across America to engage in politics and make a difference. As the US's only youth-led non-partisan & bipartisan organization focused on youth political engagement, the YACU seeks to give everyone -- no matter their party, ideology, or background -- a chance to make change. We're hosting special events, creating a nationwide network of local chapters, and fostering bipartisan discussions daily.</p>
+		<p>The Young Americans Coalition for Unity (YACU) is a 501(c)(3) nonprofit organization dedicated to engaging youth in politics and promoting unity and bipartisanship. As the US's only youth-led nonpartisan & bipartisan organization focused on this unique combined mission of youth and unity, the YACU seeks to give everyone -- no matter their party, ideology, or background -- a chance to share their political beliefs, get involved, and find common ground. We're hosting special events, creating a nationwide network of local chapters, and fostering bipartisan discussions daily.</p>
 		<p>At the YACU, you can volunteer, lead, make new friends, and pursue opportunities only our coalition can provide. Whatever you believe, wherever you are, and why ever you're here: the YACU welcomes you.</p>
 		<br><br>
 		<a-row class="strip" style="background-color: rgb(29, 50, 87); height: 450px;">
@@ -17,7 +17,7 @@
 		</a-row>
 		<br><br>
 		<h2>Our Board</h2>
-		<p>The Board of the YACU is comprised of leaders of major teams at the YACU. Board members are either founders of the organization or carefully selected and elected to their position by the other board members to fulfill a vacancy. The Board works to lead each of their respective teams, as well as drive the organization as a whole, making decisions that affect the social structure.</p>
+		<p>The Leadership Board of the YACU is comprised of leaders of major teams at the YACU. Board members are comprised of directors, officers, and representatives elected to their position by other board members or all the members of the YACU. The Board works to lead the organization as a whole, making decisions that drive the future of the YACU.</p>
 		<br>
 		<a-list :loading="$fetchState.pending" :data-source="board" style="text-align: left;">
 			<a-list-item slot="renderItem" slot-scope="item, index">
