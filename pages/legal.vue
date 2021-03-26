bdc46a9d07b54eccda8393930e59545d0d752de4
Derek Kaplan <thederekkaplan@protonmail.com>
2021-03-25 20:52:39 -0400

    Updated Code of Conduct link

--- pages/legal.vue
+++ pages/legal.vue
@@ -1,7 +1,7 @@
 <template>
 	<a-layout-content>
 		<h1>Legal Information</h1>
-		<p>To learn more about the YACU and its structure, please view our <a href="https://bylaws.yacu.org">bylaws</a> and our internal <a href="https://docs.google.com/document/d/e/2PACX-1vQHKoxfKOBv5nYuJWQGkH5y0nMWMdL-_1gN9QOOfTwS3yd3BWi4ZoZwJ5It3ZQqjR8U2SxfWXU8yKGs/pub">Code of Conduct</a>. For further questions regarding the content on this page, please email <a href="mailto:legal@yacu.org">legal@yacu.org</a>.</p>
+		<p>To learn more about the YACU and its structure, please view our <a href="https://bylaws.yacu.org">bylaws</a> and our internal <a href="https://docs.google.com/document/d/e/2PACX-1vTbNe3mYBkZz2bn3Z_FEiNSSQlHd2cT0Eev034-ReknNNKKlQsSI7FXx4bBd-jU4xGuEhWZnTqP7lsj/pub">Code of Conduct</a>. For further questions regarding the content on this page, please email <a href="mailto:legal@yacu.org">legal@yacu.org</a>.</p>
 		<object data="/Terms%20of%20Use.pdf" width="100%" height="600px">
 			<h3><a href="/Terms%20of%20Use.pdf"><a-icon type="file-pdf"/> Terms of Use</a></h3>
 		</object>
